72af90b7b74eab46202288e59f8a53762b991ceb
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-03 21:46:13 +0530

    [Anjaneya]Added: Empty space in each Element

---
+++ TikTakToe program/TikTakToe program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TikTakToe_program
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            char[] board = createBoard();
+        }
+
+        private static char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[1] = ' ';
+            }
+            return board;
+        }
+    }
+}
---
+++ TikTakToe program/TikTakToe program/TikTakToeGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTakToe_program
+{
+    class TikTakToeGame
+    {
+        int i;
+    }
+}
